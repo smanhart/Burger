209e66d8f4e1ab08f5b27ccb3d3c6fe17a3c7499
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2019-01-08 10:38:34 -0700

    set up files and connection

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Double Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Mushroom and Swiss Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon and Cheddar Burger");
